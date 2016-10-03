881635dd2dd1388514ed544cae2df3f3bfbadc79
Neantine <4sth4rtee@gmail.com>
2016-10-03 10:13:48 +0200

    some global updates between front & back

--- src/backend/app/controllers/api.js
+++ src/backend/app/controllers/api.js
@@ -13,23 +13,23 @@ module.exports = function (app) {
   app.use(bodyParser.json());
 };
 
-router.get('/images', function (req, res, next) {
+router.get('user/:userId/pictures', function (req, res, next) {
   res.status(404).send('GET ALL PICTURES NOT IMPLEMENTED YET!');
 });
 
-router.get('/images/:imageId', function (req, res, next) {
-  res.status(200).send('GET PICTURE NOT IMPLEMENTED YET!');
+// router.get('user/:userId/picture/:pictureId', function (req, res, next) {
+//   res.status(200).send('GET PICTURE NOT IMPLEMENTED YET!');
+//
+// });
 
-});
-
-router.post('/images/:nicePic', function (req, res, next) {
+router.post('user/:userId/pictures/', function (req, res, next) {
 
    let title = req.query.title;
    let fileData = req.query.fileData;
 
    let serverStorage = new ServerStorage();
 
-   let uniqueFileName = serverStorage.saveFile(title, fileData);
+   let uniqueFileName = serverStorage.saveFile(fileData);
 
    res.status(201).send('PICTURE STORED WITH ID ');//, uniqueID);
 
