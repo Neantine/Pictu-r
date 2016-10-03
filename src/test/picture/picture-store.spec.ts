2fef3a8fc469b91ddecb189f294e9ef722c3d6d2
Tribilik <y.tronel-peyroz@outlook.fr>
2016-10-03 09:14:07 +0200

    add of picture-store.spec.ts forgotten in previous commit

---
+++ src/test/picture/picture-store.spec.ts
@@ -0,0 +1,112 @@
+import {Picture} from "../../app/picture/picture";
+import {PictureStore} from "../../app/picture/picture-store";
+import {PictureModule} from "../../app/picture/picture.module";
+import {TestBed, async, inject, fakeAsync, tick} from "@angular/core/testing";
+import {BaseRequestOptions, Http, RequestMethod, ResponseOptions, Response} from "@angular/http";
+import {MockBackend} from "@angular/http/testing";
+
+describe('PictureStore', () => {
+
+  beforeEach(() => {
+
+    this.FileReaderBackup = window['FileReader'];
+  });
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        PictureModule
+      ],
+      providers: [
+        BaseRequestOptions,
+        MockBackend,
+        {
+          provide: Http,
+          useFactory: (backend, options) => new Http(backend, options),
+          deps: [MockBackend, BaseRequestOptions]
+        }
+      ]
+    }).compileComponents();
+  }));
+
+  afterEach(() => {
+    window['FileReader'] = this.FileReaderBackup;
+  });
+
+  it('should upload a picture{title:test, fileData:{data encoded in base 64}', fakeAsync(inject([MockBackend,PictureStore], (backend,pictureStore) => {
+   let picture =  new Picture({
+     title: 'toto',
+     fileData: 'data:image/jpg;base64,IMAGE_DATA'
+   });
+
+    let connectionCountSpy;
+
+    connectionCountSpy = jasmine.createSpy('connectionCount');
+    /* Mock backend. */
+    backend.connections.subscribe(connection => {
+      connectionCountSpy();
+      expect(connection.request.method).toEqual(RequestMethod.Post);
+      expect(connection.request.url).toEqual('/api/v1/users/1/pictures');
+      expect(connection.request.json()).toEqual({
+        title: 'toto',
+        fileData: 'data:image/jpg;base64,IMAGE_DATA'
+      });
+      connection.mockRespond(new Response(new ResponseOptions({
+        status:201,
+      body: {
+        id: 'IMAGE_ID',
+        title: 'toto',
+        url: '/uploads/IMAGE_ID.jpg'
+      }})));
+    });
+
+    pictureStore.uploadPicture(picture);
+
+    expect(connectionCountSpy.calls.count()).toEqual(1);
+  })));
+
+
+  it('should upload a handle error', fakeAsync(inject([MockBackend,PictureStore], (backend,pictureStore) => {
+    let error;
+    let picture =  new Picture({
+      title: 'toto',
+      fileData: 'data:image/jpg;base64,IMAGE_DATA'
+    });
+    let connectionCountSpy;
+
+    connectionCountSpy = jasmine.createSpy('connectionCount');
+
+    /* Mock backend. */
+    backend.connections.subscribe(connection => {
+      connectionCountSpy();
+      expect(connection.request.method).toEqual(RequestMethod.Post);
+      expect(connection.request.url).toEqual('/api/v1/users/1/pictures');
+      expect(connection.request.json()).toEqual({
+        title: 'toto',
+        fileData: 'data:image/jpg;base64,IMAGE_DATA'
+      });
+      connection.mockRespond(new Response(new ResponseOptions({
+        status: 500
+      })));
+    });
+
+    pictureStore.uploadPicture(picture).catch(_error => error = _error);
+
+    tick();
+
+    expect(connectionCountSpy.calls.count()).toEqual(1);
+
+    //TODO change to pass the code
+    expect(error).toBeTruthy();
+
+  })));
+
+  xit('should return Promise.reject(null) if method handleFileSelect(null)', inject([], () => {
+  }));
+
+  xit('should return Promise.reject(null) if method handleFileSelect(file) with file.type.match("image.*") ', inject([], () => {
+  }));
+
+
+
+})
