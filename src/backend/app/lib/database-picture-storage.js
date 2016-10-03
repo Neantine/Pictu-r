bf8409f408b88b43b073c87ae900ddb190e76f08
dekkaa <jdathey@hotmail.com>
2016-10-03 12:01:16 +0200

    Add files according to the database storage

--- src/backend/app/lib/database-picture-storage.js
+++ src/backend/app/lib/database-picture-storage.js
@@ -1,35 +1,37 @@
 const Picture = require('../models/picture');
 
 
-let PictureDbService = function() {
-};
-/*
-Adding a new picture to the database
- */
-PictureDbService.prototype.addPicture = function({id,title,fileStore}, cb) {
-  Picture.create({ pictureId: id, pictureTitle: title, pictureFileStore: fileStore }, function (err, small) {
-    if (err) return handleError(err);
-  })
-};
-/*
-Retrieve all the picture from the database
- */
-PictureDbService.prototype.findAllPicture = function(cb) {
-  Picture.find({}, function(err, pictures) {
-    if(err) return cb(err, null);
-    cb(null, pictures);
-  });
+class PictureDbService {
 
-};
-/*
-Retrieve a specific picture by id
- */
-PictureDbService.prototype.findPictureById = function(id, cb) {
-  Picture.findOne({ pictureId: id }, function(err, pictures) {
-    if(err) return cb(err, null);
-    cb(null, pictures);
-  });
-};
+  constructor() {
+  }
 
+  addPicture({id, title, fileStore}, cb) {
+    Picture.create({pictureId: id, pictureTitle: title, pictureFileStore: fileStore}, function (err, small) {
+      if (err) return handleError(err);
+    })
+  };
+
+  /*
+   Retrieve all the picture from the database
+   */
+  findAllPicture(cb) {
+    Picture.find({}, function (err, pictures) {
+      if (err) return cb(err, null);
+      cb(null, pictures);
+    });
+
+  };
+
+  /*
+   Retrieve a specific picture by id
+   */
+  findPictureById(id, cb) {
+    Picture.findOne({pictureId: id}, function (err, pictures) {
+      if (err) return cb(err, null);
+      cb(null, pictures);
+    });
+  };
+}
 
 module.exports = PictureDbService;
--- src/backend/test/lib/database-picture-storage.spec.js
+++ src/backend/test/lib/database-picture-storage.spec.js
@@ -17,46 +17,38 @@ describe("Database Picture", function () {
    });
    */
 
+
   it("can be saved a picture", function (done) {
-    supertest(app)
-      .post('/images/nicePic')
-      .expect(400)
-      .end(function (err, res) {
-        pictureDbService.addPicture({
-          pictureId: '123',
-          pictureTitle: "My nice pic",
-          typeFileStore: "local"
-        }, done);
-        console.log("My nice pic is store in Database");
+
+    pictureDbService.addPicture({
+      pictureId: '123',
+      pictureTitle: "My nice pic",
+      typeFileStore: "local"
+    })
+      .then(() => {
+
       });
+
   });
 
+
   it("can be saved another picture", function (done) {
-    supertest(app)
-      .post('/images/nicePic')
-      .expect(200)
-      .end(function (err, res) {
-        pictureDbService.addPicture({
-          pictureId: '456',
-          pictureTitle: "My other nice pic",
-          typeFileStore: "local"
-        }, done);
-        console.log("My other nice pic is store in Database");
+
+    pictureDbService.addPicture({
+      pictureId: '456',
+      pictureTitle: "My other nice pic",
+      typeFileStore: "local"
+    })
+      .then(() => {
+
       });
   });
 
   it("can be listed", function (done) {
-    supertest(app)
-      .get('/images')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, done.fail)
-      .end(function (err, res) {
-        pictureDbService.findAllPicture(function (err, pictures) {
-          expect(err).to.not.exist;
-          expect(pictures).to.have.length(2);
-          done();
-        });
+    pictureDbService.findAllPicture(function (err, pictures) { })
+      .then((err, pictures) => {
+        expect(err).to.not.exist;
+        expect(pictures).to.have.length(2);
       })
-  });
+  })
 });
