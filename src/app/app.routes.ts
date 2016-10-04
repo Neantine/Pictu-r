ef22628f705da36088a1395c20240de452c0662e
tkmremi <tkmremi@gmail.com>
2016-10-04 14:17:53 +0200

    Corrections on app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
-import { PictureUploaderComponent } from "./picture/picture-uploader/picture-uploader.component";
-import { PictureGalleryComponent } from "./picture/picture-gallery/picture-gallery.component";
-import { homecomp } from "./picture/picture-home/picture-home";
+import { PictureUploaderComponent }   from  "./picture/picture-uploader/picture-uploader.component";
+import { PictureGalleryComponent }    from  "./picture/picture-gallery/picture-gallery.component";
+import { homecomp }                   from  "./picture/picture-home/picture-home";
 
 
 export const ROUTES: Routes = [
