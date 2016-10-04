a131c793117b85a87f42570b70badbd23a2c05b8
tkmremi <tkmremi@gmail.com>
2016-10-04 13:38:05 +0200

    add home routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 
 import { PictureUploaderComponent } from "./picture/picture-uploader/picture-uploader.component";
 import { PictureGalleryComponent } from "./picture/picture-gallery/picture-gallery.component";
+import { AppComponent } from "./picture/picture-home/picture-home";
 
 
 export const ROUTES: Routes = [
   { path: '', component: PictureGalleryComponent },
   { path: 'pictures', component: PictureUploaderComponent },
+  { path: 'index', component: AppComponent },
 ];
