ce1a3371f3c3c2f3b064df4a2309b0cba3ff1194
tkmremi <tkmremi@gmail.com>
2016-10-04 13:12:28 +0200

    Style & Picture home added

---
+++ src/app/picture/picture-home/picture-home.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+import {bootstrap} from '@angular/platform-browser-dynamic';
+
+@Component({
+  selector: 'material-app',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {}
+
