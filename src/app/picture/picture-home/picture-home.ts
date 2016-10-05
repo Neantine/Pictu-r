59a83ea5620b69d71d7eac8d87a1f7912c7d89e6
tkmremi <tkmremi@gmail.com>
2016-10-05 10:29:55 +0200

    I DONT KNOW WHAT THE FUCK IS GOING ON THESE ERRORS

--- src/app/picture/picture-home/picture-home.ts
+++ src/app/picture/picture-home/picture-home.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'home-app',
-  templateUrl: require('./picture-home.html'),
+  templateUrl: './picture-home.html',
   styles  : [ require('./picture-home.css') ]
 })
 
