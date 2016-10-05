3aff5997e7d3c48a29047e0bcda88bdff0572613
tkmremi <tkmremi@gmail.com>
2016-10-05 15:43:46 +0200

    Fix

--- src/app/picture/picture-home/picture-home.component.ts
+++ src/app/picture/picture-home/picture-home.component.ts
@@ -4,7 +4,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'home-app',
   styles  : [ require('./picture-home.component.css') ],
-  templateUrl: require('picture-home.component.html')
+  templateUrl: 'picture-home.component.html'
 })
 
 export class HomeComponent {}
