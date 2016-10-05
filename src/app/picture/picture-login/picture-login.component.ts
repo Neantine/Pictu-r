a39dd787b6e9436e1feceed9e827775fb2ca9294
Neantine <4sth4rtee@gmail.com>
2016-10-05 16:23:16 +0200

    test

--- src/app/picture/picture-login/picture-login.component.ts
+++ src/app/picture/picture-login/picture-login.component.ts
@@ -4,7 +4,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'login-app',
   styles  : [ require('./picture-login.component.css') ] ,
-  templateUrl: './picture-login.component.html'
+  templateUrl: 'picture-login.component.html'
 })
 
 export class LoginComponent {}
