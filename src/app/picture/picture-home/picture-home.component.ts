3c007aa152f4bce808da17fab8ae3208e0a95708
Neantine <4sth4rtee@gmail.com>
2016-10-05 16:15:16 +0200

    test

--- src/app/picture/picture-home/picture-home.component.ts
+++ src/app/picture/picture-home/picture-home.component.ts
@@ -4,7 +4,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'home-app',
   styles  : [ require('./picture-home.component.css') ],
-  templateUrl : 'picture-home.component.html'
+  templateUrl : './picture-home.component.html'
 })
 
 export class HomeComponent {}
