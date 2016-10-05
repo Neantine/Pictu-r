2cfc024d4f22c8d2540e6670e7ebae74bb68ad2c
Neantine <4sth4rtee@gmail.com>
2016-10-05 16:40:05 +0200

    test

--- src/app/picture/picture-home/picture-home.component.ts
+++ src/app/picture/picture-home/picture-home.component.ts
@@ -6,11 +6,6 @@ import {Component} from '@angular/core';
   styles  : [ require('./picture-home.component.css') ],
   template: require('./picture-home.component.html')
 })
-
 export class HomeComponent {
-
-  constructor() {
-
-  }
 }
 
