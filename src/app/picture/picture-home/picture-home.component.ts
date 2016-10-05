af290455d54148ff3f2e737263c2354b11644f1c
tkmremi <tkmremi@gmail.com>
2016-10-05 15:36:48 +0200

    'PAAAAAASE'

--- src/app/picture/picture-home/picture-home.component.ts
+++ src/app/picture/picture-home/picture-home.component.ts
@@ -4,7 +4,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'home-app',
   styles  : [ require('./picture-home.component.css') ],
-  templateUrl: 'picture-home.component.html'
+  templateUrl: require('picture-home.component.html')
 })
 
 export class HomeComponent {}
