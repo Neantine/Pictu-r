39a8e57271be6d485569836ba670142493931166
tkmremi <tkmremi@gmail.com>
2016-10-06 11:47:37 +0200

    picture.module.ts : Fixed syntax errors

--- src/app/picture/picture.module.ts
+++ src/app/picture/picture.module.ts
@@ -1,12 +1,12 @@
 
 import { NgModule } from '@angular/core';
-import { PictureUploaderComponent } from "./picture-uploader/picture-uploader.component";
-import { PictureGalleryComponent } from "./picture-gallery/picture-gallery.component";
-import { CommonModule } from "../common.module";
-import { PictureStore } from "./picture-store";
-import { PicturUrlSanitizerPipe } from "./picture-url-sanitizer";
-import { HomeComponent } from "./picture-home/picture-home.component";
-import { LoginComponent } from "./picture-login/picture-login.component";
+import { PictureUploaderComponent } from './picture-uploader/picture-uploader.component';
+import { PictureGalleryComponent } from './picture-gallery/picture-gallery.component';
+import { CommonModule } from '../common.module';
+import { PictureStore } from './picture-store';
+import { PicturUrlSanitizerPipe } from './picture-url-sanitizer';
+import { HomeComponent } from './picture-home/picture-home.component';
+import { LoginComponent } from './picture-login/picture-login.component';
 
 
 @NgModule({
